382bdd0585ebae6919136650a6c6e9c6cf39bd9c
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-22 13:40:36 +0000

    Display all bookings within a table

---
+++ Carfinance.Phoenix.Kata.Angular/app/index.js
@@ -0,0 +1,43 @@
+﻿class App extends React.Component {
+    state = {
+        bookings: []
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:52363/booking')
+            .then(res => res.json())
+            .then(bookings => this.setState({ bookings }));  
+    };
+    
+    render() {
+        const { bookings } = this.state;
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Contact Name</th>
+                            <th>Contact Number</th>
+                            <th>No of Diners</th>
+                            <th>Table Number</th>
+                            <th>Booking Time</th>
+                        </tr>
+                        {bookings.map((booking, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>{booking.contactName}</td>
+                                    <td>{booking.contactNumber}</td>
+                                    <td>{booking.numberOfPeople}</td>
+                                    <td>{booking.tableNumber}</td>
+                                    <td>{booking.bookingTime}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    };
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
